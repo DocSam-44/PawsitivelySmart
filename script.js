a2e97ccc1027e7bfcd6dd1999cf4edb1a8fe7499
DocSam-44 <sammeemayer4@gmail.com>
2026-06-04 15:21:27 -0400

    saving before making a new branch

---
+++ script.js
@@ -0,0 +1,15 @@
+// add the hero forge image to the same class as the other slideshow borders
+
+// google navbar to make the links more usable on the phone
+
+// connect the link
+
+// look into accessability design for those with colour blindness
+// maybe click a button that changes to more accessible colours
+// image tag alt to give a screen reader work
+
+// add copyright tag
+
+// add a footer
+
+// testimonials section
