4022fca8378c722f2925547893116a745799d35d
DocSam-44 <sammeemayer4@gmail.com>
2026-06-04 16:59:13 -0400

    recorded reasonings for things i chose not to include

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 // connect the link
+    // * I decided not to connect my survey submission button to anything because I didn't feel comfortable signing up for anything or connecting it to any of my personal accounts since this is a public github page and it's for school
 
 // testimonials section
     // * I decided not to add a testimonials section because i didn't think it would be a good idea to bother my clients for a school project. When I make a proper website in the future I will add some.
